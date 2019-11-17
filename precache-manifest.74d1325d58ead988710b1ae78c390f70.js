63f5eae215b4436758f60dfb5c41c9533362c779
Adrián Pérez López <a.przlopez@gmail.com>
2019-11-17 20:43:26 +0100

    Updates

---
+++ precache-manifest.74d1325d58ead988710b1ae78c390f70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480bc38b90c2d055abe2450be2c7ec27",
+    "url": "/starting-with-react/index.html"
+  },
+  {
+    "revision": "c2a27181a2d66b0474bf",
+    "url": "/starting-with-react/static/css/main.9682993b.chunk.css"
+  },
+  {
+    "revision": "34d2fada84b4db533337",
+    "url": "/starting-with-react/static/js/2.5daf6f25.chunk.js"
+  },
+  {
+    "revision": "c2a27181a2d66b0474bf",
+    "url": "/starting-with-react/static/js/main.9d5d4ce0.chunk.js"
+  },
+  {
+    "revision": "dc8e2fbb43fdc07fb95f",
+    "url": "/starting-with-react/static/js/runtime-main.55be1b61.js"
+  }
+]);
